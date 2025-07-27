3fe29ece54dba933e123175efa0bd463d573abfe
Pedro de Souza Goettert <pedrogoettert@hotmail.com>
2025-07-27 09:54:25 -0300

    add icons in navigation

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,17 +2,50 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import {
+  FaBullhorn, // Campanhas
+  FaCalendar, // Calendário
+  FaGauge, // Dashboard
+  FaGear, // Configurações,
+  FaHouse, // Home
+  FaImage, // Criativos
+  FaLightbulb, // Ideias
+} from 'react-icons/fa6';
 export function Navigation() {
   const pathName = usePathname();
 
   const links = [
-    { href: '/', label: 'Home' },
-    { href: '/dashboard', label: 'Dashboard' },
-    { href: '/campanhas', label: 'Campanhas' },
-    { href: '/criativos', label: 'Criativos' },
-    { href: '/ideias', label: 'Ideias' },
-    { href: '/calendario', label: 'Calendario' },
-    { href: '/configuracoes', label: 'Configurações' },
+    { href: '/', label: 'Home', icon: <FaHouse className="text-2xl" /> },
+    {
+      href: '/dashboard',
+      label: 'Dashboard',
+      icon: <FaGauge className="text-2xl" />,
+    },
+    {
+      href: '/campanhas',
+      label: 'Campanhas',
+      icon: <FaBullhorn className="text-2xl" />,
+    },
+    {
+      href: '/criativos',
+      label: 'Criativos',
+      icon: <FaImage className="text-2xl" />,
+    },
+    {
+      href: '/ideias',
+      label: 'Ideias',
+      icon: <FaLightbulb className="text-2xl" />,
+    },
+    {
+      href: '/calendario',
+      label: 'Calendario',
+      icon: <FaCalendar className="text-2xl" />,
+    },
+    {
+      href: '/configuracoes',
+      label: 'Configurações',
+      icon: <FaGear className="text-2xl" />,
+    },
   ];
 
   return (
@@ -25,10 +58,14 @@ export function Navigation() {
               : pathName.startsWith(link.href);
           return (
             <li
-              className={`rounded-md border-1 border-gray-300 border-solid text-xl ${isActive ? 'bg-blue-400' : 'bg-transparent'}`}
+              className={`items-center gap-2 rounded-md border-1 border-gray-300 border-solid text-xl ${isActive ? 'bg-blue-400' : 'bg-transparent'}`}
               key={link.href}
             >
-              <Link className="flex h-full w-full py-2 pl-2" href={link.href}>
+              <Link
+                className="flex h-full w-full gap-4 py-2 pl-4 focus:rounded-md"
+                href={link.href}
+              >
+                {link.icon}
                 {link.label}
               </Link>
             </li>
